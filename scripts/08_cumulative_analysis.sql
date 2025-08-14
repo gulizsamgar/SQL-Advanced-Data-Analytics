13fadedcd2224f303a8543d4cb0f80d6569dc51c
Güliz SAMĞAR <118976805+gulizsamgar@users.noreply.github.com>
2025-08-14 16:54:50 +0300

    Update 08_cumulative_analysis.sql

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -12,8 +12,25 @@ SQL Functions Used:
 ===============================================================================
 */
 
--- Calculate the total sales per month 
+-- Calculate the total sales per month
 -- and the running total of sales over time 
+SELECT
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER (PARTITION BY orde_date ORDER BY order_date) AS running_total_sales
+FROM
+(
+    SELECT 
+        DATETRUNC(month, order_date) AS order_date,
+        SUM(sales_amount) AS total_sales,
+    FROM gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(month, order_date)
+) t
+	
+
+-- Calculate the total sales per year 
+-- and the running total of sales over time, moving average price  of sales over time
 SELECT
 	order_date,
 	total_sales,
@@ -29,3 +46,6 @@ FROM
     WHERE order_date IS NOT NULL
     GROUP BY DATETRUNC(year, order_date)
 ) t
+
+
+
